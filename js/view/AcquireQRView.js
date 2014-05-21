72150634cc4507672b0259d71ff9dc848b57857e
GrEx0 <grex77@gmail.com>
2014-05-21 16:21:39 +0200

    asd

--- js/view/AcquireQRView.js
+++ js/view/AcquireQRView.js
@@ -6,5 +6,27 @@ AcquireQR_View = Backbone.View.extend({
         render: function(){
             this.$el.html( this.template() );
             return this.el;
-        }
+        },
+        events: {
+        	"click #startScan" : "doScan"
+    	},
+ 
+ 		doScan: function() {
+        console.log('scanning');
+        
+        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+        scanner.scan( function (result) { 
+
+            alert("We got a barcode\n" + 
+            "Result: " + result.text + "\n" + 
+            "Format: " + result.format + "\n" + 
+            "Cancelled: " + result.cancelled);  
+
+           console.log("Scanner result: \n" +
+                "text: " + result.text + "\n" +
+                "format: " + result.format + "\n" +
+                "cancelled: " + result.cancelled + "\n");
+            document.getElementById("results").innerHTML = result.text;
+            console.log(result);
     });
