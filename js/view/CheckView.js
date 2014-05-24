3da5fd761f8a376a0c4557ab4f9c8075be6e4af1
GrEx0 <grex77@gmail.com>
2014-05-24 22:26:51 +0200

    A

--- js/view/CheckView.js
+++ js/view/CheckView.js
@@ -6,7 +6,9 @@ Check_View = Backbone.View.extend({
         },
         render: function(){
             this.$el.html( this.template(this.model.toJSON()) );
+            console.log("Rendering Check_view");
             return this.el;
+           
         }
     });
 
