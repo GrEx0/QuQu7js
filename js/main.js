fab4e5e9a6e26f7000172541154ddaadaa6714dd
GrEx0 <grex77@gmail.com>
2014-05-30 11:02:35 +0200

    funziona PUSh

--- js/main.js
+++ js/main.js
@@ -11,9 +11,7 @@ var AppRouter = Backbone.Router.extend({
  
     initialize: function () {
         $('header').html(new Header_View().render());
-        window.ticket = new Ticket();
-        pushNotification = window.plugins.pushNotification;
-		pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"408316694165","ecb":"app.onNotificationGCM"});
+        window.ticket = new Ticket();         
     },
 
     routes: {
@@ -47,41 +45,6 @@ var AppRouter = Backbone.Router.extend({
         this.currentView = view;
          
         return view;
-    },
-    
-    // result contains any message sent from the plugin call
-	successHandler: function(result) {
-   		 alert('Callback Success! Result = '+result);
-	},
-	
-	errorHandler:function(error) {
-    		alert(error);
-	},
-
-	onNotificationGCM: function(e) {
-        switch( e.event )
-        {
-            case 'registered':
-                if ( e.regid.length > 0 )
-                {
-                    console.log("Regid " + e.regid);
-                    alert('registration id = '+e.regid);
-                }
-            break;
- 
-            case 'message':
-              // this is the actual push notification. its format depends on the data model from the push server
-              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-            break;
- 
-            case 'error':
-              alert('GCM error = '+e.msg);
-            break;
- 
-            default:
-              alert('An unknown GCM event has occurred');
-              break;
-        }
     }
  
  /*   before: function (callback) {
@@ -106,8 +69,110 @@ var AppRouter = Backbone.Router.extend({
 
 /* --- Start main --- */
 
-tpl.loadTemplates(['Start', 'AcquireQR', 'Check','Header'], function () {
-   app = new AppRouter();
-   Backbone.history.start();
-});
+document.addEventListener('deviceready', onDeviceReady, true);
+function onDeviceReady() {
+		console.log("DeviceReady");
+		tpl.loadTemplates(['Start', 'AcquireQR', 'Check','Header'], function () {
+   			app = new AppRouter();
+   			Backbone.history.start();
+   
+	});
+	var pushNotification;
+	try 
+				{ 
+                	pushNotification = window.plugins.pushNotification;
+                	if (device.platform == 'android' || device.platform == 'Android') {
+						console.log("registro android");
+                    	pushNotification.register(successHandler,errorHandler, {"senderID":"408316694165","ecb":"onNotificationGCM"});		// required!
+					} else {
+						$("#app-status-ul").append('<li>registering iOS</li>');
+                    	pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});	// required!
+                	}
+                }
+				catch(err) 
+				{ 
+					txt="There was an error on this page.\n\n"; 
+					txt+="Error description: " + err.message + "\n\n"; 
+					alert(txt);
+					console.log(txt);
+				}
+}
+// handle APNS notifications for iOS
+function onNotificationAPN(e) {
+                if (e.alert) {
+                     console.log(e.alert);
+                     navigator.notification.alert(e.alert);
+                }
+                    
+                if (e.sound) {
+                    var snd = new Media(e.sound);
+                    snd.play();
+                }
+                
+                if (e.badge) {
+                    pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
+                }
+            }
+            
+            // handle GCM notifications for Android
+            function onNotificationGCM(e) {
+                console.log("EVENT RECEIVED"+e.event);
+                
+                switch( e.event )
+                {
+                    case 'registered':
+					if ( e.regid.length > 0 )
+					{
+						// Your GCM push server needs to know the regID before it can push to this device
+						// here is where you might want to send it the regID for later use.
+						console.log("regID = " + e.regid);
+					}
+                    break;
+                    
+                    case 'message':
+                    	// if this flag is set, this notification happened while we were in the foreground.
+                    	// you might want to play a sound to get the user's attention, throw up a dialog, etc.
+                    	if (e.foreground)
+                    	{
+							console.log("INLINE NOTIFICATION");
+							
+							// if the notification contains a soundname, play it.
+							var my_media = new Media("/android_asset/www/"+e.soundname);
+							my_media.play();
+						}
+						else
+						{	// otherwise we were launched because the user touched a notification in the notification tray.
+							if (e.coldstart)
+								console.log("COLDSTART NOTIFICATION");
+							else
+							console.log("BACKGROUND NOTIFICATION");
+						}
+							
+					console.log('MESSAGE -> MSG: ' + e.payload.message + '\n');
+                    break;
+                    
+                    case 'error':
+						console.log('ERROR -> MSG:' + e.msg + '\n');
+                    break;
+                    
+                    default:
+						console.loh('EVENT -> Unknown, an event was received and we do not know what it is');
+                    break;
+                }
+            }
+            
+            function tokenHandler (result) {
+                console.log('token: '+ result +'\n');
+                // Your iOS push server needs to know the token before it can push to this device
+                // here is where you might want to send it the token for later use.
+            }
+			
+            function successHandler (result) {
+                console.log('success:'+ result +'\n');
+            }
+            
+            function errorHandler (error) {
+                console.log('error:'+ error +'\n');
+            }
+
 
