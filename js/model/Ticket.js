0c6cb7dc5087c735b33c173b0d6b1a4befabf982
carrello <maildipietroilacqua@gmail.com>
2014-06-02 12:42:34 +0200

    iug

--- js/model/Ticket.js
+++ js/model/Ticket.js
@@ -3,6 +3,7 @@ Ticket = Backbone.Model.extend({
         "id":null,
         "data":"",
         "center":"N/D",
+        "centerPosition":"0,0",
         "ticketNumber":"N/D",
         "operation":"",
         "waitingTime":"N/D",
--- js/view/CheckView.js
+++ js/view/CheckView.js
@@ -5,10 +5,74 @@ Check_View = Backbone.View.extend({
              this.model.on("change", this.render);
             console.log("Check.html template caricato");
             
+            
+            
+            
+            //test geolocalizzazione
+           /* navigator.geolocation.getCurrentPosition(
+        	
+            function(position) {
+                $('.location', this.el).html(position.coords.latitude + ',' +position.coords.longitude);
+                
+            },
+            function() {
+                alert('Error getting location');
+            });
+            */
+            //displaymappa
+
+
+            
+            
+            
+            //variabili
+           
+            
+            //test calcolo distanza google distance matrix
+            /*
+            
+             var service;
+            var destination;
+            service = new google.maps.DistanceMatrixService();
+            destination= new google.maps.LatLng(43.5, -80.5);
+            
             navigator.geolocation.getCurrentPosition(
             
             
-            estimateWalkingTime
+         function (position){
+        	
+        	
+        	service.getDistanceMatrix (
+        		
+        		{
+        			//parametri : 
+        			origins : [google.maps.LatLng(position.coords.latitude, position.coords.longitude)],
+        			destinations : //get(this.model.centerPosition) ,
+        			               [destination],
+        			travelMode : google.maps.TravelMode.WALKING
+        			
+        			
+        		},
+        		    
+        		    //callback :
+        		    //this.model.walkingTime = response.rows[0].elements[0].distance.text
+        		    
+        		    //$('.est', this.el).html(response.rows[0].elements[0].distance.value)
+        		    function(response,status){
+
+                    if(status==google.maps.DistanceMatrixStatus.OK) {
+   
+                    $('.estimation', this.el).html(response.rows[0].elements[0].duration.value);
+    
+                    } else {
+                    alert("Error: " + status);
+                    }
+        	
+                    }
+        		
+        	);
+        }
+
          
              ,
             
@@ -17,10 +81,71 @@ Check_View = Backbone.View.extend({
             }
             
             );
+      
+        },*/
+       
+          //test calcolo distanza google directions
             
-             
+            
+            var service;
+            var destination;
+            service = new google.maps.DirectionsService();
+            destination= new google.maps.LatLng('43.5', '-80.5');
+            
+            navigator.geolocation.getCurrentPosition(
+            
+            
+         function (position){
+         	
+         	
+        	
+        	
+        	service.route (
+        		
+        		{
+        			//parametri : 
+        			origins : google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+        			destinations : //get(this.model.centerPosition) ,
+        			               destination,
+        			travelMode : google.maps.TravelMode.WALKING
+        			
+        			
+        		},
+        		    
+        		    //callback :
+        		    
+        		    function(response,status){
+        		    	
+        		    	alert('dentro');
+
+                    if(status==google.maps.DirectionsStatus.OK) {
+   
+                    //$('.estimation', this.el).html(response.duration.value);
+                    alert('we'+response);
+    
+                    } else {
+                    alert("Error: " + status);
+                    }
+        	
+                    }
+        		
+        	);
+        }
+
+         
+             ,
+            
+             function() {
+                alert('Error getting location');
+            }
+            
+            );
+      
         },
         
+      //test di calcolo distanza mapquest
+        
+    
         render: function(){
             this.$el.html( this.template(this.model.toJSON()) );
             console.log("Rendering Check_view");
@@ -28,27 +153,10 @@ Check_View = Backbone.View.extend({
             
            
         },
+        
+     
        
-        estimateWalkingTime: function (position){
-        	
-        	
-        	google.maps.DistanceMatrixService.getDistanceMatrix (
-        		
-        		{
-        			//parametri :
-        			origins : google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-        			destinations : this.centro.coordinate ,
-        			travelmode : google.maps.TravelMode.WALKING,
-        			avoidHighways : false,
-        			avoidTolls : false
-        			
-        		},
-        		    
-        		    //callback :
-        		    walkingTime = response.rows[0].elements[0].distance.text
-        		
-        	);
-        }
+        
   
     });
 
