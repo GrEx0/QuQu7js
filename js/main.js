65743a2c9ac53c7fb2bcb96489aca20bb4cb7fcf
carrello <maildipietroilacqua@gmail.com>
2014-06-02 15:28:47 +0200

    kb

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@ function onDeviceReady() {
    		window.ticket = new Ticket(); 
 		window.link = new link();
    		window.regid='test';
-   		window.url ="http://localhost/QuQu7js/ServerSide/";
+   		window.url ="http://localhost:8888/QuQu7js/ServerSide/";
    		//"http://ququ7.altervista.org/QuQu7js/ServerSide/";
    
 	});
