747ebb4dca63146058122bd7d2f0838a64c23aa5
carrello <maildipietroilacqua@gmail.com>
2014-06-08 15:14:57 +0200

    sad

--- ServerSide/Operatore/operatore.php
+++ ServerSide/Operatore/operatore.php
@@ -1,16 +1,84 @@
 <?php
-		//echo('se vedi  questo echo e non gli altri non carica require');
-		//echo(realpath('../operations.php'));
+		
 		require_once(realpath('../operations.php'));	
-		$db = db_connect();
-		echo('ho fatto db_connect');
-		if ($db){
-			echo('sono dentro if');
-			$operations = showOperations($db);
-			// codifica dei risultati
-            echo json_encode($operations);
-			echo('end if \n');
+		
+		//dichiarazione invariabili
+		$idCentro=8;
+		$idSportello=1;
+		$foo="bella";
+		
+		
+		
+ if (isset($_GET['operation'])){
+ 	
+	switch ($_GET['operation']) {
+		
+		
+		//funzione displaya 
+		
+		case 'getNumber':
 			
-		} else{ echo('ha saltato if');}
+			$db = db_connect();
+		
+		if ($db){
+			
+			
+			printf("<li> %s </li>",$idSportello);
+			
+		}
+			break;
+			
+			case 'getOper':
+			
+			$db = db_connect();
+		
+			if ($db){
+			$query = "SELECT operazioni.Descrizione FROM operazioni,sportelli	 WHERE sportelli.id_operazione_ext=operazioni.Id AND sportelli.Id=$idSportello";
+			$result = $db->query($query);
+			while ($record = $result->fetch_array(MYSQLI_ASSOC)) {
+				printf("<li><a>%s</a></li>",$record['Descrizione']);
+			}
+		}
+			break;
+			
+			
+			//funzione prendi le operazioni da mettere nella combobox
+		
+		case 'getOperazioni':
+		
+		$db = db_connect();
+		
+		if ($db){
+		
+			$query="SELECT operazioni.Id,operazioni.Descrizione FROM operazioni WHERE 1";
+			
+			$result=$db->query($query); 
+			
+			
+			while ($row = mysqli_fetch_assoc($result)) {
+    $record[] = $row;
+}
+			// codifica dei risultati
+            echo json_encode($record);
+			
+			
+		} else{ echo('connessione fallita');}
+		
+		break;
+		
+		
+		case 'changeOperazione':
+			
+			break;
+			
+		case 'avantiNumero':
+			
+			break;
+		
+		
+		
+ }
+ 
+ }
 
 ?>
