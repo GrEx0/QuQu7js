7afb986964d6ea0b39e0c3336d6e063d609f4df1
carrello <maildipietroilacqua@gmail.com>
2014-06-08 18:22:02 +0200

    we

--- ServerSide/Operatore/operatore.js
+++ ServerSide/Operatore/operatore.js
@@ -1,24 +1,45 @@
 $(document).ready(function(){
 	
+	
+	
 	$('#cc').combobox({
+		
+		
 		url:'operatore.php?&operation=getOperazioni',
-		valueField:'Descrizione',
-		textField: 'Descrizione'
+		valueField:'Id',
+		textField: 'Descrizione',
+		onSelect : function(record){
+			
+			//cambia operazione
+			
+			$('#cc').combobox('setValue', record.Id);
+		    
+		    console.log(record.Descrizione);
+		    
+		    $(this).load("operatore.php?&operation=changeOperazione&NuovaOperazione="+record.Id);
+		    
+		    
+		    $("#oper").load("operatore.php?&operation=getOper");//refresh
+		  
+		}
 		
 	});
 	
-	
-	
+		
 	
 	$("#number").load("operatore.php?&operation=getNumber");
 	$("#oper").load("operatore.php?&operation=getOper");
 	
-	$("#changeButton").click(function(){
+	
+	//chiama prossimo numero	
+   $("#next").click(function(){
 		
+		(this).load("operatore.php?&operation=avantiNumero");
 		
-		console.log($( '#cc' ).val());
+		$("#number").load("operatore.php?&operation=getNumber");//refresh
 		
 		});
+
 
 });
 
