8c041acba9b622f915070cd4d907a2c0be5b3ff0
GrEx0 <grex77@gmail.com>
2014-06-24 23:40:13 +0200

    logo ok e GCM

--- ServerSide/index.php
+++ ServerSide/index.php
@@ -3,6 +3,7 @@
 	if (isset($_GET["id"]) && isset($_GET['regid']))
 	{
 		require_once 'operations.php';
+		require_once 'GCM.php';
 			
 			$db = db_connect();
 				
@@ -11,6 +12,8 @@
 					 	if ($answer){
 					 				InsertUser($_GET["id"],$_GET["regid"],$db);
 									echo(json_encode($answer));
+									$gcm = new GCM();
+									$gcm->send_notification($_GET["regid"], "Notifica push - Utente inserito nel server");
 					 			 	}
 				    } 	
 					else { echo("errore");}
